fa669b60e921e686468f3556e95b1aaa68d82453
Muhammaddiyor Tohirov <opensource@takhirov.uz>
2024-12-21 19:28:14 +0500

    ref: simplify useHighlight by spreading code block options

--- src/modules/shiki/composables/useHighlight.ts
+++ src/modules/shiki/composables/useHighlight.ts
@@ -3,11 +3,7 @@ import { highlighter, getCodeBlockDetails } from "@/modules/shiki";
 
 export const useHighlight = (...elements: Ref<HTMLElement>[]): void => {
   elements.forEach((element) => {
-    const { lang, theme, snippet } = getCodeBlockDetails(element.value);
-
-    element.value.innerHTML = highlighter.codeToHtml(snippet, {
-      lang,
-      theme
-    });
+    const { snippet, ...options } = getCodeBlockDetails(element.value);
+    element.value.innerHTML = highlighter.codeToHtml(snippet, options);
   });
 };
